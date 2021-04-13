a6c3eb21c28207f85dadd0d9c80a651d2cf21d57
Jason Chen <wind50321@gmail.com>
2021-04-13 21:20:54 +0800

    Basic version

---
+++ password.py
@@ -0,0 +1,13 @@
+password = 'a123456'
+i = 3 # 剩餘機會
+while i > 0:
+    i = i - 1
+    pwd = input('請輸入密碼： ')
+    if pwd == password:
+        print('登入成功！')
+        break # 逃出迴圈
+    else:
+        if i > 0:
+            print('密碼錯誤，還剩', i , '次機會！')
+        else:
+            print('密碼錯誤，帳號已被鎖定！')
